02a7ea14729cb3c5ff1717281fd69b78d547e8ea
mshamir11 <mshamir11@gmail.com>
2023-04-11 06:33:21 +0530

    Fixed unit tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+/**
+ * This is a test for the App component.
+ */
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Welcome to my page/i);
   expect(linkElement).toBeInTheDocument();
 });
