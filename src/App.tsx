b36bbc87c6043ceefbd87deaf5b2b61ceb5b1ae4
mshamir11 <mshamir11@gmail.com>
2023-04-11 07:14:12 +0530

    Configured tailwind fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Welcome to my page
+      {/* Center the title */}
+      <div className="flex justify-center">
+        {/* Make the title a bit bigger */}
+        <h1 className="text-4xl">
+          Welcome to my page
+        </h1>
+      </div>
     </div>
   );
 }
