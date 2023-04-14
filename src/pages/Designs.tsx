2498772756f7dd62051352e32f26b723242af9d3
mshamir11 <mshamir11@gmail.com>
2023-04-14 21:57:31 +0530

    Fix padding in the design page

--- src/pages/Designs.tsx
+++ src/pages/Designs.tsx
@@ -3,7 +3,7 @@ import { image1, image2, image3 } from "../data/DesignData";
 
 export const Designs = () => {
 	return (
-		<div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+		<div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
 			<div className="flex flex-col mb-5">
 				<h1 data-testid="design-title" className="flex text-4xl  justify-center mb-2">
 					Designs{" "}
